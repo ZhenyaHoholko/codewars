ed4d3c358a4dd3eaceeb1ec742d1440f18541d12
ZhenyaHoholko <zhenya.hoholko@gmail.com>
2023-11-20 23:12:58 +0300

    done

---
+++ Tasks/6q_WriteNumberInExpandedForm.js
@@ -0,0 +1,17 @@
+// Task:
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+
+function expandedForm(num) {
+  return String(num)
+    .split("")
+    .map((item, index) => +item * Math.pow(10, String(num).length - index - 1))
+    .filter((el) => el)
+    .join(" + ");
+}
+console.log(expandedForm(31031));
