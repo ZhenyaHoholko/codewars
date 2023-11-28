e07aa14740c7c1fd80dd511fc7365b8ba25be8cc
ZhenyaHoholko <zhenya.hoholko@gmail.com>
2023-11-28 22:51:00 +0300

    done

---
+++ Tasks/8q_SumMixedArray.js
@@ -0,0 +1,8 @@
+// Task:
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+function sumMix(x) {
+  return x.reduce((sum, current) => sum + Number(current), 0);
+}
+console.log(sumMix([9, 3, "7", "3"]));
