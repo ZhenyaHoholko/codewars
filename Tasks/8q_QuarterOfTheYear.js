3c029258a6647cacbfe5dbb286710bb0e898353c
ZhenyaHoholko <zhenya.hoholko@gmail.com>
2023-12-03 23:24:10 +0300

    Done better than before

--- Tasks/8q_QuarterOfTheYear.js
+++ Tasks/8q_QuarterOfTheYear.js
@@ -6,15 +6,7 @@
 // 1 <= month <= 12
 
 const quarterOf = (month) => {
-  if (month <= 3) {
-    return 1;
-  } else if (month <= 6) {
-    return 2;
-  } else if (month <= 9) {
-    return 3;
-  } else {
-    return 4;
-  }
+  return Math.ceil(month / 3);
 };
 
 console.log(quarterOf(8));
